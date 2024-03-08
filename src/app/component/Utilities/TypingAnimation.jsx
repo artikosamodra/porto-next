e1325d3ba8e8babfea5beb5d002636b4aa4cd2c1
artikosamodra <artikosamudra@gmail.com>
2024-03-08 16:40:41 +0700

    Structure About Page

--- src/app/component/Utilities/TypingAnimation.jsx
+++ src/app/component/Utilities/TypingAnimation.jsx
@@ -2,12 +2,11 @@
 "use client";
 import { useEffect, useState } from "react";
 
-const texts = ["Teks pertama", "Teks kedua", "Teks ketiga"];
-const typingDelay = 100;
+const typingDelay = 200;
 const erasingDelay = 50;
-const newTextDelay = 2000;
+const newTextDelay = 1100;
 
-const TypingAnimation = () => {
+const TypingAnimation = ({ texts }) => {
   const [textIndex, setTextIndex] = useState(0);
   const [text, setText] = useState("");
   const [isTyping, setIsTyping] = useState(true);
@@ -17,16 +16,19 @@ const TypingAnimation = () => {
       () => {
         if (isTyping) {
           if (text.length < texts[textIndex].length) {
-            setText((prev) => prev + texts[textIndex].charAt(prev.length));
+            setText((prev) => prev + texts[textIndex][prev.length]);
           } else {
             setIsTyping(false);
-            setTimeout(() => setIsTyping(true), newTextDelay);
+            setTimeout(() => {
+              setIsTyping(true);
+            }, newTextDelay);
           }
         } else {
           if (text.length > 0) {
             setText((prev) => prev.slice(0, -1));
           } else {
-            setTextIndex((prev) => (prev + 1) % texts.length);
+            const nextIndex = (textIndex + 1) % texts.length;
+            setTextIndex(nextIndex);
           }
         }
       },
@@ -34,11 +36,11 @@ const TypingAnimation = () => {
     );
 
     return () => clearInterval(interval);
-  }, [text, textIndex, isTyping]);
+  }, [text, textIndex, isTyping, texts]);
 
   return (
-    <div className="mt-20 text-4xl">
-      <h1 className="pl-5">Halo{text}</h1> {/* Menetapkan padding kiri */}
+    <div className="text-3xl mt-4">
+      <h1 className="">F{text}</h1>
     </div>
   );
 };
--- src/app/page/About/page.jsx
+++ src/app/page/About/page.jsx
@@ -1,9 +1,32 @@
 import TitlePage from "@/app/component/Utilities/TitlePage";
+import TypingAnimation from "@/app/component/Utilities/TypingAnimation";
+import Image from "next/image";
 
 const About = () => {
+  const textsRandom = ["rontend Developer"];
   return (
     <section>
       <TitlePage titlePage="ABOUT" />
+      <div className="grid grid-cols-3 py-20">
+        <div className="col-span-1 flex justify-center items-center">
+          <Image
+            src="/picture.jpg"
+            width={300}
+            height={300}
+            className="rounded-full w-56 h-56 shadow-xl"
+          />
+        </div>
+        <div className="col-span-2">
+          <h1 className="font-bold text-5xl">ARTIKO FAJAR SAMODRA</h1>
+          <TypingAnimation texts={textsRandom} />
+          <div className="pt-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            fugiat dignissimos consectetur perferendis quaerat repellendus ullam
+            sequi quod quas, deserunt magni autem nam nesciunt voluptas
+            veritatis officiis aspernatur! Quas, asperiores!
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
