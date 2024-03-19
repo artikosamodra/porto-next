1934731417da037a20f8caeae9a817052389974a
artikosamodra <artikosamudra@gmail.com>
2024-03-19 16:19:34 +0700

    add konten and setting expertise section

--- src/app/component/Utilities/ExpertiseCard.jsx
+++ src/app/component/Utilities/ExpertiseCard.jsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 
 const ExpertiseCard = ({ iconTech, nameTech, descTech }) => {
   return (
-    <div>
-      <Image src={iconTech} alt="Icon" width={100} height={100} />
-      <h1>{nameTech}</h1>
-      <p>{descTech}</p>
+    <div className="p-5 bg-stone-100 rounded-md">
+      <Image src={iconTech} alt="Icon" width={70} height={70} />
+      <h1 className="font-bold text-slate-600 text-lg pt-2 pb-1 pl-2">
+        {nameTech}
+      </h1>
+      <p className="font-semibold text-slate-400 text-sm pl-2">{descTech}</p>
     </div>
   );
 };
--- src/app/page/About/Expertise.jsx
+++ src/app/page/About/Expertise.jsx
@@ -5,86 +5,86 @@ const MyExpertise = () => {
   return (
     <section className="px-40 py-10">
       <TitlePage titlePage="My Expertise" />
-      <div className="grid grid-cols-4">
+      <div className="grid grid-cols-4 gap-4">
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/html.png"
           nameTech="HTML"
-          descTech="Lorem10"
+          descTech="HTML is a markup language for creating structured and formatted web page content and structure."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/css.png"
           nameTech="CSS"
-          descTech="Lorem10"
+          descTech="CSS controls the appearance of web pages by styling, coloring, laying out, and formatting HTML elements."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/javascript.png"
           nameTech="JavaScript"
-          descTech="Lorem10"
+          descTech="JavaScript adds interactivity and dynamic behavior to web pages by manipulating HTML and reacting to user actions."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/tailwind.png"
           nameTech="Tailwind"
-          descTech="Lorem10"
+          descTech="Tailwind is a utility-first CSS framework that streamlines web development by providing pre-built, customizable classes."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/react.png"
           nameTech="React"
-          descTech="Lorem10"
+          descTech="React is a JavaScript library for building user interfaces, emphasizing component-based architecture and efficient rendering"
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/next.png"
           nameTech="NextJS"
-          descTech="Lorem10"
+          descTech="Next.js is a React framework for building server-side rendered and statically generated web applications with ease."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/node.png"
           nameTech="NodeJS"
-          descTech="Lorem10"
+          descTech="Node.js is a JavaScript runtime environment that allows running server-side JavaScript applications outside of a web browser."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/express.png"
           nameTech="ExpressJS"
-          descTech="Lorem10"
+          descTech="Express is a minimalist and flexible Node.js web application framework that simplifies building robust APIs and web servers."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/aws.png"
+          nameTech="AWS"
+          descTech="AWS (Amazon Web Services) is a cloud computing platform providing a wide range of services for building scalable applications."
+        />
+        <ExpertiseCard
+          iconTech="/img/skill/boostrap.png"
           nameTech="Boostrap"
-          descTech="Lorem10"
+          descTech="Bootstrap is a popular front-end framework for developing responsive and mobile-first websites with pre-built CSS and JavaScript components."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/git.png"
           nameTech="Git"
-          descTech="Lorem10"
+          descTech="Git is a distributed version control system for tracking changes in source code during software development collaboratively."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/Unity.png"
           nameTech="Unity 3D"
-          descTech="Lorem10"
+          descTech="Unity is a cross-platform game engine used for developing interactive 2D and 3D experiences for various devices."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
-          nameTech="AR Foundation"
-          descTech="Lorem10"
-        />
-        <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/mysql.png"
           nameTech="MySQL"
-          descTech="Lorem10"
+          descTech="MySQL is an open-source relational database management system for storing and managing structured data efficiently."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/csharp.png"
           nameTech="C#"
-          descTech="Lorem10"
+          descTech="C# is a powerful, object-oriented programming language developed by Microsoft for building a variety of applications."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/kotlin.png"
           nameTech="Kotlin"
-          descTech="Lorem10"
+          descTech="Kotlin is a modern, statically-typed programming language for developing Android apps, web applications, and server-side development."
         />
         <ExpertiseCard
-          iconTech="/picture.jpg"
+          iconTech="/img/skill/php.png"
           nameTech="PHP"
-          descTech="Lorem10"
+          descTech="PHP is a server-side scripting language used for web development to create dynamic and interactive websites."
         />
       </div>
     </section>
