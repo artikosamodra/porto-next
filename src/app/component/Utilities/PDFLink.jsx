c7521b01235e5358257ec124275bbce555d269fa
artikosamodra <artikosamudra@gmail.com>
2024-03-19 17:16:23 +0700

    minor bug

--- src/app/component/Utilities/PDFLink.jsx
+++ src/app/component/Utilities/PDFLink.jsx
@@ -1,13 +1,10 @@
 const PDFLink = () => {
-    return(
-        <Link href={pdfSrc}>
+  return (
+    <Link href={pdfSrc}>
       <a target="_blank" rel="noopener noreferrer">
         <Image src={thumbnailSrc} width={100} height={100} alt="Thumbnail" />
       </a>
     </Link>
   );
 };
-    )
-}
-
-export default PDFLink
+export default PDFLink;
